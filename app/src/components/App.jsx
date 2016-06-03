90ee45f9cd1d6c587fd85333b9d38cb035a28f5d
rrequero <rareq1987@gmail.com>
2016-06-03 11:17:18 +0200

    add vectortile client

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,10 +1,18 @@
 'use strict';
 
 import React, {Component} from 'react';
+import PelagosClient  from '../lib/pelagosClient';
 
+var url = "http://localhost:8000/ui_tests/data/testtiles/-90,-45,-45,-22.5";
 class App extends Component {
 
   render() {
+    let promise = new PelagosClient().obtainTile(url);
+    promise.then(function(data){
+      console.log('Todo ocorrecto', data);
+    }, function(err){
+      console.error('Error', err);
+    });
     return (
       <div>
         <p>Hello!</p>
