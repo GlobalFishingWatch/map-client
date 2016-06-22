4412aa381cc7ab693d8527fb9d600283ffcf2d93
Adrián <aloha@adrianperez.info>
2016-06-22 15:48:01 +0200

    satellite as default basemap

--- app/src/components/map.jsx
+++ app/src/components/map.jsx
@@ -145,7 +145,8 @@ class Map extends Component {
 						    />
 						  }
         googleMapElement={
-								<GoogleMap ref="map" defaultZoom = {3} defaultCenter = {{lat: 0, lng: 0}}
+								<GoogleMap ref="map" defaultZoom = {3} defaultCenter = {{lat: 0, lng: 0}} defaultMapTypeId = {google.maps.MapTypeId.SATELLITE}
+
 					      onIdle = {
 					        this.onIdle.bind(this)
 					      }
