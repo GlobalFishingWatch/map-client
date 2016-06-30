af9226373d350e436f606b268983d73ae7006e96
Tiago Garcia <tiagojsag@gmail.com>
2016-06-30 06:40:56 +0200

    Add basic auth to production env

--- config/environments/production.js
+++ config/environments/production.js
@@ -2,10 +2,14 @@
 
 const path = require('path');
 const express = require('express');
+const basicAuth = require('basic-auth-connect');
 
 const indexPath = path.join(process.cwd(), 'dist/index.html');
 
 module.exports = function(app) {
+  if (process.env.AUTH_USER && process.env.AUTH_PASSWORD) {
+    app.use(basicAuth(process.env.AUTH_USER, process.env.AUTH_PASSWORD));
+  }
   app.use(express.static(path.join(process.cwd(), 'dist')));
   app.get('*', (req, res) => {
     res.sendFile(indexPath);
