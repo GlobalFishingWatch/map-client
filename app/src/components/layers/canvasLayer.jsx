ae10ebcb841d8b5421d95d192259927defa27f7d
Tiago Garcia <tiagojsag@gmail.com>
2016-07-07 22:21:42 +0200

    Change tile URL to match specification

--- app/src/components/layers/canvasLayer.jsx
+++ app/src/components/layers/canvasLayer.jsx
@@ -66,7 +66,7 @@ class CanvasLayer {
     let coordRec = this.getNormalizedCoord(coord, zoom);
     var zoomDiff = zoom + 8 - Math.min(zoom + 8, 16);
     if(coordRec){
-      new PelagosClient().obtainTile(`https://storage.googleapis.com/vizzuality-staging/data/${coordRec.x},${coordRec.y},${zoom}`).then(function(data) {
+      new PelagosClient().obtainTile(`https://storage.googleapis.com/vizzuality-staging/data/${zoom},${coordRec.x},${coordRec.y}`).then(function(data) {
         this.drawTile(canvas, zoom, data);
       }.bind(this));
     }
