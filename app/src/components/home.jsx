34a57057317e79893ffd733fabc8b0e0fa24e9d9
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-07-08 12:52:16 +0200

    update styles and content

--- app/src/components/home.jsx
+++ app/src/components/home.jsx
@@ -38,7 +38,7 @@ class Home extends Component {
           </div>
         </div>
       </section>
-      <section className={home.section2}>
+      <section className={home.infostudy}>
         <div>
           <h2>CASE ESTUDY</h2>
           <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -50,16 +50,57 @@ class Home extends Component {
         </div>
         <div></div>
       </section>
-      <section className={home.section3}>
+      <section className={home.infomap}>
         <div>
           <h2>THE MAP</h2>
           <h3>Lorem ipsum dolor sit amet</h3>
           <p>Morbi porttitor massa id bibendum varius. Etiam vitae pulvinar nisi, vel fringilla libero. Nulla consequat sodales lectus.</p>
           <p>
-            <a href="#" className={home.btn_primary}>EXPLORE MAP</a>
+            <a href="#" className={home.c_btn_primary}>EXPLORE MAP</a>
           </p>
         </div>
       </section>
+      <section className={home.success_story}>
+        <h2>Success Stories</h2>
+        <div className={home.gallery_info}>
+            <div className={home.image_text}>
+                <div className={home.img}></div>
+                <p className={home.info_text}>
+                  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.
+    <span className={home.title_info}><b>Name</b> / Position</span>
+    <a className={home.link_more} href="#">find out more</a>
+                </p>
+            </div>
+            <div className={home.image_text}>
+                <div className={home.img}></div>
+                <p className={home.info_text}>
+                  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.
+    <span className={home.title_info}><b>Name</b> / Position</span>
+    <a className={home.link_more} href="#">find out more</a>
+                </p>
+            </div>
+            <div className={home.image_text}>
+                <div className={home.img}></div>
+                <p className={home.info_text}>
+                  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.
+    <span className={home.title_info}><b>Name</b> / Position</span>
+    <a className={home.link_more} href="#">find out more</a>
+                </p>
+            </div>
+        </div>
+      </section>
+      <footer>
+        <div className={home.logos_footer}>
+          <div className={home.partner_footer}>
+            <span>Founding Partners</span>
+          </div>
+          <div className={home.sponsor_footer}>
+            <span>Lead Sponsor</span>
+          </div>
+        </div>
+        <div className={home.nav_footer}></div>
+        <div className={home.project_name_footer}></div>
+      </footer>
     </div>
 
   }
