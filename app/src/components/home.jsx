3ba3d4babbc494336b8937547ae6db2d22c73ce6
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-07-08 13:51:03 +0200

    update styles and content

--- app/src/components/home.jsx
+++ app/src/components/home.jsx
@@ -93,12 +93,38 @@ class Home extends Component {
         <div className={home.logos_footer}>
           <div className={home.partner_footer}>
             <span>Founding Partners</span>
+            <img src="app/assets/partners/google.png"></img>
+            <img src="app/assets/partners/google.png"></img>
+            <img src="app/assets/partners/google.png"></img>
           </div>
           <div className={home.sponsor_footer}>
             <span>Lead Sponsor</span>
           </div>
         </div>
-        <div className={home.nav_footer}></div>
+        <div className={home.nav_footer}>
+          <ul>
+            <li>map</li>
+            <li>news</li>
+            <li>how to</li>
+            <li>about</li>
+          </ul>
+          <ul>
+            <li>terms of use</li>
+            <li>privacy policy</li>
+            <li>contact us</li>
+          </ul>
+          <ul>
+            <li>Follow us</li>
+            <li>ICONS</li>
+          </ul>
+          <div className={home.input_footer}>
+            <span>SUSCRIBE TO NEWS</span>
+              <div className={home.c_input_button} >
+                <input type="email" placeholder="Email"></input>
+                <button class="button--addOnRight" className={home.button_addOnRight}>icon</button>
+            </div>
+          </div>
+        </div>
         <div className={home.project_name_footer}></div>
       </footer>
     </div>
