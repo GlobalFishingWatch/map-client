be60473e182433cad0ab53584dcde48eb279353c
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-07-21 11:43:22 +0200

    add function to go section case-study

--- app/src/components/cover_page.jsx
+++ app/src/components/cover_page.jsx
@@ -1,12 +1,17 @@
 'use strict';
 import React, {Component} from "react";
+import $ from "jquery";
 import ButtonBoxSlider from "./button_box_slider";
 import cover_page from "../../styles/components/c_cover_page.scss";
 import boxtriangle from "../../assets/icons/box_triangle.svg";
 import box_triangle from "../../styles/components/c_box_triangle.scss";
 
 class CoverPage extends Component {
-
+  gosection(){
+  	$('html, body').animate({
+  		scrollTop: $("#case_study").offset().top
+  	}, 1000);
+  }
   render() {
     return <section className={cover_page.c_cover_page}>
       <div>
@@ -16,7 +21,7 @@ class CoverPage extends Component {
         <p>Global Fishing Watch, a partnership of Oceana, SkyTruth and Google, enables anyone with an Internet connection to see global fishing activity worldwide in near real-time - for free. It s a powerful tool that will hold our leaders accountable for maintaining abundant oceans and show consumers where - - and by whom -- their fish is being caught.</p>
         <div className={cover_page.footer_header}>
           <ButtonBoxSlider></ButtonBoxSlider>
-          <div className={box_triangle.c_box_triangle}>
+          <div className={box_triangle.c_box_triangle} onClick={this.gosection}>
             <div className={box_triangle.triangle_min}></div>
           </div>
           <div>Brought to you by:</div>
