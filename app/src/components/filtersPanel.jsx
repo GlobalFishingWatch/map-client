9811335690b025c679131d78c3b4f3ef70488855
Adrián <aloha@adrianperez.info>
2016-07-22 12:35:59 +0200

    layers component

--- app/src/components/filtersPanel.jsx
+++ app/src/components/filtersPanel.jsx
@@ -2,25 +2,10 @@ import React, {Component} from "react";
 import filtersPanel from "../../styles/components/c_filter_panel.scss";
 
 export default function (props) {
-  let layers = [];
-  if (props.layers) {
-    for (let i = 0, length = props.layers.length; i < length; i++) {
-      layers.push(
-        <li key={i}>
-          <label>
-            <input type="checkbox" checked={props.layers[i].visible}
-                   onChange={() => props.onToggle(props.layers[i])}></input>
-            {props.layers[i].title}
-          </label>
-        </li>
-      );
-    }
-  }
   return (
     <div className={filtersPanel.filtersPanel}>
-      <ul>
-        {layers}
-      </ul>
+      <label for="ISOfilter">Vessel flag (ISO code)
+      <input type="text" id="ISOfilter" onChange={(e) => props.onChange(e.target.value)} /></label>
     </div>
   );
 }
