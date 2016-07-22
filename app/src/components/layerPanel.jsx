0830aef4e7cb5b17ff6de63f725d877907ebc706
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-07-22 12:50:19 +0200

    add accordion and new styles [WIP]

--- app/src/components/layerPanel.jsx
+++ app/src/components/layerPanel.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { Accordion, AccordionItem } from 'react-sanfona';
 import layerPanel from "../../styles/components/c_layer_panel.scss";
 
 export default function (props) {
@@ -18,9 +19,19 @@ export default function (props) {
   }
   return (
     <div className={layerPanel.layerPanel}>
-      <ul>
-        {layers}
-      </ul>
+      <Accordion allowMultiple={false}>
+        {["Search Vessels", "BaseMap", "Layers"].map((item) => {
+          return (
+            <AccordionItem title={`${ item }`} key={item} className={layerPanel.title_accordion}>
+              <div className={layerPanel.content_accordion}>
+                {item === "Search Vessels" ? <p>Hola</p> : null}
+                {item === "BaseMap" ? <p>Hola</p> : null}
+                {item === "Layers" ? <p>{layers}</p>: null}
+              </div>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
     </div>
   );
 }
