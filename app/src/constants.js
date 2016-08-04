6c4ebfc0cb8e1826d1f8c40838e0b143ccc0cde2
Tiago Garcia <tiagojsag@gmail.com>
2016-08-04 19:32:12 +0200

    Change default dates

--- app/src/constants.js
+++ app/src/constants.js
@@ -34,7 +34,7 @@ export const TIMELINE_DEFAULT_START_DATE = 1420070400000; // 01/01/2015
 export const TIMELINE_DEFAULT_END_DATE = 1451516400000; // 31/12/2015
 
 export const TIMELINE_TOTAL_DATE_EXTENT = [new Date(Date.UTC(2015, 0, 1)), new Date(Date.UTC(2016, 0, 1))];
-export const TIMELINE_INNER_EXTENT = [new Date(Date.UTC(2015, 5, 1)), new Date(Date.UTC(2015, 7, 1))];
+export const TIMELINE_INNER_EXTENT = [new Date(Date.UTC(2015, 0, 1)), new Date(Date.UTC(2015, 0, 15))];
 
 export const MIN_ZOOM_LEVEL = 2;
 export const MAX_ZOOM_LEVEL = 12;
