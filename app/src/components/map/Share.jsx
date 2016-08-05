629c260d3feb59723179b89b33eba224c788b50e
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-08-05 14:09:40 +0200

    Lets the user copy the URL into their clipboard

--- app/src/components/map/Share.jsx
+++ app/src/components/map/Share.jsx
@@ -3,9 +3,43 @@ import styles from '../../../styles/components/map/c-share.scss';
 
 class Share extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      copied: false,   // If the input's content has been copied
+      copyError: false // If the copy action failed
+    };
+  }
+
+  /**
+   * Copy the input's content into the clipboard and set the state's "copied"
+   * attribute to true. If the action failed, set the "copyError" attribute to
+   * true. After few milliseconds, the "copied" attribute is resetted to false
+   * to display a quick visual feedback.
+   *
+   * @param {object} e - click event on the button
+   */
   onCopy(e) {
     e.preventDefault();
-    /* TODO */
+
+    this.refs.input.select();
+
+    let error = false;
+    try {
+      if (!document.execCommand('copy')) error = true;
+    } catch (err) {
+      error = true;
+    }
+
+    this.setState({
+      copied: !error,
+      error
+    });
+
+    /* We want the copied state to only last few millisecond to display a visual
+     * feedback */
+    if (!error) setTimeout(() => this.setState({ copied: false }), 1000);
   }
 
   /**
@@ -42,6 +76,11 @@ class Share extends Component {
     }
 
     const url = this.getURLWithWorkspace();
+    const copyError = (
+      <p className={styles['copy-error']}>
+        Sorry, the link couldn't be copied. Please right click on the input and copy it manually.
+      </p>
+    );
 
     return (
       <div className={styles['c-share']}>
@@ -50,9 +89,12 @@ class Share extends Component {
           Copy and paste the link into an email or IM
         </p>
         <form>
-          <input className={styles['url-input']} type="text" readOnly value={url} />
-          <button className={styles['copy-button']} type="submit" onClick={() => this.onCopy()}>Copy</button>
+          <input className={styles['url-input']} type="text" readOnly value={url} ref="input" />
+          <button className={styles['copy-button']} type="submit" onClick={e => this.onCopy(e)}>
+            {this.state.copied ? 'Copied!' : 'Copy'}
+          </button>
         </form>
+        {this.state.error && copyError}
       </div>
     );
   }
