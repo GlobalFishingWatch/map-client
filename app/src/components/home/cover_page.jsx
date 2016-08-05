61d60a0fd73a6fad75c49b5e79d539027eefe128
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-08-05 12:20:37 +0200

    add slick on cover page

--- app/src/components/home/cover_page.jsx
+++ app/src/components/home/cover_page.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import Slider from 'react-slick';
 import ButtonBoxSlider from './button_box_slider';
 import CoverPageStyle from '../../../styles/components/c-cover-page.scss';
 import BoxTriangleStyle from '../../../styles/components/c-box-triangle.scss';
@@ -12,25 +13,50 @@ class CoverPage extends Component {
   }
 
   render() {
-    return (<section className={CoverPageStyle['c-cover-page']}>
-      <div>
-        <h1>
-          The first global view of commercial fishing activity
-        </h1>
-        <p>Global Fishing Watch, a partnership of Oceana, SkyTruth and Google, enables anyone with an Internet
-          connection to see global fishing activity worldwide in near real-time - for free. It s a powerful tool that
-          will hold our leaders accountable for maintaining abundant oceans and show consumers where - - and by whom --
-          their fish is being caught.
-        </p>
-        <div className={CoverPageStyle['footer-header']}>
-          <ButtonBoxSlider />
-          <div className={BoxTriangleStyle['c-box-triangle']} onClick={this.gosection}>
-            <div className={BoxTriangleStyle['triangle-min']}></div>
+    return (
+      <div className={CoverPageStyle['c-cover-page']}>
+        <div>
+          <Slider
+            dots
+            arrows={false}
+            draggable={false}
+            infinite
+            speed={500}
+            fade
+          >
+            <div>
+              <h1>
+                The first global view of commercial fishing activity
+              </h1>
+              <p>Global Fishing Watch, a partnership of Oceana, SkyTruth and Google, enables anyone with an Internet
+                connection to see global fishing activity worldwide in near real-time
+                - for free. It s a powerful tool that
+                will hold our leaders accountable for maintaining abundant oceans and show consumers
+                where - - and by whom -- their fish is being caught.
+              </p>
+            </div>
+            <div>
+              <h1>
+                The second global view of commercial fishing activity
+              </h1>
+              <p>Global Fishing Watch, a partnership of Oceana, SkyTruth and Google, enables anyone with an Internet
+                connection to see global fishing activity worldwide in near real-time
+                - for free. It s a powerful tool that
+                will hold our leaders accountable for maintaining abundant oceans and show consumers
+                where - - and by whom -- their fish is being caught.
+              </p>
+            </div>
+          </Slider>
+          <div className={CoverPageStyle['footer-header']}>
+            <ButtonBoxSlider />
+            <div className={BoxTriangleStyle['c-box-triangle']} onClick={this.gosection}>
+              <div className={BoxTriangleStyle['triangle-min']}></div>
+            </div>
+            <div>Brought to you by:</div>
           </div>
-          <div>Brought to you by:</div>
         </div>
       </div>
-    </section>);
+    );
   }
 }
 
