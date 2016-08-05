d8d93996257ac7bcb4c0cda9710e738d74999f45
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-08-05 13:51:48 +0200

    update style form the map section homepage

--- app/src/components/home/info_map.jsx
+++ app/src/components/home/info_map.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import InfoMapContentStyle from '../../../styles/components/c-info-map-content.scss';
-import ButtonStyle from '../../../styles/components/c-button.scss';
+import ButtonStyle from '../../../styles/components/c-button-arrow.scss';
 import BoxTriangleWhiteStyle from '../../../assets/icons/box_triangle_white.svg';
 
 class InfoMap extends Component {
 
   render() {
-    return (<div className={InfoMapContentStyle.c_info_map_content}>
+    return (<div className={InfoMapContentStyle['c-info-map-content']}>
       <h2>THE MAP</h2>
       <h3>Lorem ipsum dolor sit amet</h3>
       <p>Morbi porttitor massa id bibendum varius. Etiam vitae pulvinar nisi, vel fringilla libero. Nulla consequat
         sodales lectus.
       </p>
       <p>
-        <a href="map" className={ButtonStyle.c_btn_primary}>
+        <a href="map" className={ButtonStyle['c-btn-primary-arrow']}>
           <img src={BoxTriangleWhiteStyle} alt="Explore map" />EXPLORE MAP</a>
       </p>
     </div>);
