a1bab3ef28c7e609631187b22f012e0b9b7636cc
nerik <erik.escoffier@gmail.com>
2016-08-08 13:35:13 +0200

    added duration picker to timeline

---
+++ app/src/components/map/DurationPicker.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import css from '../../../styles/components/map/c-durationpicker.scss';
+
+class DurationPicker extends Component {
+
+  getHumanizedDuration(extent) {
+    const innerDelta = moment(extent[1])
+      .diff(moment(extent[0]));
+    return moment.duration(innerDelta).humanize();
+  }
+
+  getWidth(extentPx) {
+    return `${extentPx[1] - extentPx[0]}px`;
+  }
+
+  getLeft(extentPx) {
+    return `${extentPx[0]}px`;
+  }
+
+  render() {
+    const humanizedDuration = this.getHumanizedDuration(this.props.extent);
+    const style = {
+      width: this.getWidth(this.props.extentPx),
+      left: this.getLeft(this.props.extentPx)
+    };
+
+    return (
+      <div style={style} className={css['c-durationpicker']}>
+        <div className={css['c-durationpicker-text']}>
+          {humanizedDuration}
+        </div>
+      </div>
+    );
+  }
+}
+
+DurationPicker.propTypes = {
+  extent: React.PropTypes.array,
+  extentPx: React.PropTypes.array
+};
+
+export default DurationPicker;
