54ac786fa7dd75533f9aabf500ad1482649dbaf3
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-08-09 13:10:15 +0200

    update styles from blog page detail, fix cover and layer

--- app/src/components/shared/CoverSecondary.jsx
+++ app/src/components/shared/CoverSecondary.jsx
@@ -19,6 +19,10 @@ class CoverSecondary extends Component {
         </p>
         <div className={CoverPage['footer-header']}>
           <div>
+<<<<<<< e5302c4a886429963586b06d3a9b95331105e06e
+=======
+            <p className={CoverPage['text-ldf-footer']}>Brought to you by:</p>
+>>>>>>> update styles from blog page detail, fix cover and layer
             <img className={CoverPage['ldf-logo']} src={LogoLDF} alt="logo"></img>
           </div>
         </div>
