a0596730b8b60d76ac052c932cebf7392f5dd490
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-08-09 13:36:52 +0200

    delete text from cover pages footer

--- app/src/components/shared/CoverPrimary.jsx
+++ app/src/components/shared/CoverPrimary.jsx
@@ -19,7 +19,6 @@ class CoverPrimary extends Component {
         </p>
         <div className={CoverPage['footer-header']}>
           <div>
-            <p className={CoverPage['text-ldf-footer']}>Brought to you by:</p>
             <img className={CoverPage['ldf-logo']} src={LogoLDF} alt="logo"></img>
           </div>
         </div>
