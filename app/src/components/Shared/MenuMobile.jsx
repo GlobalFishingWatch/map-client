41ced17055041cc6d7595ad966e1308166abe563
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-08-11 10:13:43 +0200

    delete duplicate styles and delete files

--- app/src/components/Shared/MenuMobile.jsx
+++ app/src/components/Shared/MenuMobile.jsx
@@ -3,31 +3,31 @@ import menuMobile from '../../../styles/components/c-mobile-menu.scss';
 
 class MenuMobile extends Component {
   render() {
-    return (<div className={menuMobile.c_mobile_menu}>
+    return (<div className={menuMobile['c-mobile-menu']}>
       <ul>
         <li>Map</li>
         <li>News</li>
-        <ul className={menuMobile.submenu_mobile}>
+        <ul className={menuMobile['submenu-mobile']}>
           <li>Newsletter</li>
           <li>Articles</li>
           <li>Blog</li>
           <li>Case Studies</li>
         </ul>
         <li>How to</li>
-        <ul className={menuMobile.submenu_mobile}>
+        <ul className={menuMobile['submenu-mobile']}>
           <li>FAQ</li>
           <li>Tutorials</li>
           <li>Definitions</li>
           <li>About</li>
         </ul>
-        <ul className={menuMobile.submenu_mobile}>
+        <ul className={menuMobile['submenu-mobile']}>
           <li>The Project</li>
           <li>Partners</li>
           <li>Contact Us</li>
         </ul>
       </ul>
-      <span className={menuMobile.button_login}>LOG IN</span>
-      <p className={menuMobile.forgot_text}>Forgot your password? Not a member</p>
+      <span className={menuMobile['button-login']}>LOG IN</span>
+      <p className={menuMobile['forgot-text']}>Forgot your password? Not a member</p>
     </div>);
   }
 }
