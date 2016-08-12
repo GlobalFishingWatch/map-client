f0d3417fee8d1fc163e9890f39646855b467a16c
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2016-08-12 13:59:49 +0200

    renamed LinkTo to Rhombus component

---
+++ app/src/components/Shared/Rhombus.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import RhombusStyles from '../../../styles/components/shared/c-rhombus.scss';
+
+// This component is used to replace the rhombus element around the website.
+// Feel free to customize it and improve it if need.
+class Rhombus extends Component {
+
+  getClassName() {
+    const direction = this.props.direction ? RhombusStyles[this.props.direction] : '';
+    const color = this.props.color ? RhombusStyles[this.props.color] : '';
+
+    return `${RhombusStyles['c-link-to']} ${direction} ${color}`;
+  }
+
+  render() {
+    return (
+      <div className={this.getClassName()} onClick={this.props.onClick}>
+        <span className={RhombusStyles.rhombus}></span>
+        {this.props.children &&
+          <span className={RhombusStyles.text}>{this.props.children}</span>}
+      </div>
+    );
+  }
+}
+
+Rhombus.propTypes = {
+  children: React.PropTypes.node,
+  direction: React.PropTypes.string,
+  color: React.PropTypes.string,
+  onClick: React.PropTypes.func
+};
+
+export default Rhombus;
