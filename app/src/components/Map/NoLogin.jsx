4f997a54fdd771ca93f29f5887f08cf8b76b55eb
Tiago Garcia <tiagojsag@gmail.com>
2016-08-12 15:29:08 +0200

    No login modal styling

--- app/src/components/Map/NoLogin.jsx
+++ app/src/components/Map/NoLogin.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import styles from '../../../styles/components/map/c-share.scss';
+import styles from '../../../styles/components/map/c-no-login.scss';
 
 class NoLogin extends Component {
 
@@ -9,10 +9,12 @@ class NoLogin extends Component {
       <div className={styles['c-share']}>
         <h2 className={styles.title}>Login required</h2>
         <p className={styles.intro}>
-          You must log in to view the Map. If you are a registered user, <a onClick={this.props.login}>log in here</a>
+          You must log in to view the Map. If you are a registered user,&nbsp;
+          <a className={styles.link_underline} onClick={this.props.login}>log in here</a>
         </p>
         <p>
-          If you are not a registered user, learn more about Global Fishing Watch <Link to="/">here</Link>
+          If you are not a registered user, learn more about Global Fishing Watch&nbsp;
+          <Link className={styles.link_underline} to="/">here</Link>
         </p>
       </div>
     );
