77cfa1a3241b97d52e94ac1b69e9ed832f21e620
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-08-17 18:09:11 +0200

    fix eslint errors

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import home from '../../styles/index.scss';
 import backMobile from '../../styles/components/c-mobile-menu.scss';
 import Footer from './Shared/Footer';
-import CaseStudySlider from './Home/CaseStudySlider';
 import InfoWeb from './Home/InfoWeb';
 import CoverPage from './Home/CoverPage';
 import InfoMap from './Home/InfoMap';
-import SuccessStoryGallery from './Home/SuccessStoryGallery';
 
 class Home extends Component {
   constructor(props) {
@@ -23,7 +21,7 @@ class Home extends Component {
       {/*<section id="case_study" className={home.infostudy}>
         <CaseStudySlider />
       </section>*/}
-      <InfoWeb/>
+      <InfoWeb />
       <section className={home.infomap}>
         <InfoMap />
       </section>
