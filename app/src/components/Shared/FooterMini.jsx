47047306d4263ea60138578345d81876dbb591ad
nerik <erik.escoffier@gmail.com>
2016-08-19 14:27:48 +0200

    toggle full Footer

--- app/src/components/Shared/FooterMini.jsx
+++ app/src/components/Shared/FooterMini.jsx
@@ -3,6 +3,7 @@ import styles from '../../../styles/components/shared/c-footer-mini.scss';
 import logooceana from '../../../assets/logos/oceana_logo.png';
 import logosky from '../../../assets/logos/skytruth_logo.jpg';
 import logogoogle from '../../../assets/logos/google_logo.png';
+import Footer from './Footer';
 
 class FooterMini extends Component {
 
@@ -18,33 +19,44 @@ class FooterMini extends Component {
   }
 
   render() {
-    if (this.state.footerExpanded) {
-      return (
-        <div>Show full footer, don't remove mini one (we still need the link to hide the footer)?</div>
-      );
-    }
-
     /**
      * TODO:
-     * Don't hide mini footer because we need the link to toggle the full footer
      * Need to review the link and the legal terms
      */
 
-    return (
-      <footer className={styles['c-footer-mini']}>
-        <div className={styles.partners}>
-          <img className={styles.partner} src={logooceana} alt="oceana logo" />
-          <img className={styles.partner} src={logosky} alt="skytruth logo" />
-          <img className={styles.partner} src={logogoogle} alt="google logo" />
+    let expandedFooter;
+    if (this.state.footerExpanded) {
+      expandedFooter = (
+        <div
+          style={{
+            position: 'absolute',
+            bottom: '38px' }}
+        >
+          <Footer />
         </div>
+      );
+    }
 
-        <ul className={styles.links}>
-          <li><a onClick={() => this.toggleFooter()}>Show Footer</a></li>
-          <li>CARTO</li>
-          <li>Map data ©2016 Google, INEGI</li>
-          <li>Terms of Use</li>
-        </ul>
-      </footer>
+    let toggleLabel = (this.state.footerExpanded) ? 'Hide Footer' : 'Show Footer';
+
+    return (
+      <div>
+        {expandedFooter}
+        <footer className={styles['c-footer-mini']}>
+          <div className={styles.partners}>
+            <img className={styles.partner} src={logooceana} alt="oceana logo" />
+            <img className={styles.partner} src={logosky} alt="skytruth logo" />
+            <img className={styles.partner} src={logogoogle} alt="google logo" />
+          </div>
+
+          <ul className={styles.links}>
+            <li><a onClick={() => this.toggleFooter()}>{toggleLabel}</a></li>
+            <li>CARTO</li>
+            <li>Map data ©2016 Google, INEGI</li>
+            <li>Terms of Use</li>
+          </ul>
+        </footer>
+      </div>
     );
   }
 }
