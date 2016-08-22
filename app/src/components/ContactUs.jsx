02f07d51248f634480009cfd3ffb0093f7f546de
nerik <erik.escoffier@gmail.com>
2016-08-22 12:18:46 +0200

    name and email filled in by default
    in the contact form, when logged in

--- app/src/components/ContactUs.jsx
+++ app/src/components/ContactUs.jsx
@@ -12,7 +12,12 @@ class ContactUs extends Component {
       <div className={contactStyle['c-contact']}>
         <Header />
         <div className={baseStyle.wrap}>
-          <ContactUsForm onFormSubmit={this.props.submitForm} contactStatus={this.props.contactStatus} />
+          <ContactUsForm
+            onFormSubmit={this.props.submitForm}
+            contactStatus={this.props.contactStatus}
+            defaultUserName={this.props.defaultUserName}
+            defaultUserEmail={this.props.defaultUserEmail}
+          />
         </div>
       </div>
       <Footer />
@@ -22,7 +27,9 @@ class ContactUs extends Component {
 
 ContactUs.propTypes = {
   contactStatus: React.PropTypes.number,
-  submitForm: React.PropTypes.func
+  submitForm: React.PropTypes.func,
+  defaultUserName: React.PropTypes.string,
+  defaultUserEmail: React.PropTypes.string
 };
 
 export default ContactUs;
