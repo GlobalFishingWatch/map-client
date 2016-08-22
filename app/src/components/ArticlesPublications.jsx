3945bc69c844987772d3fb42f9e7ded1316e99e3
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2016-08-22 17:30:26 +0200

    publications & articles redesign

--- app/src/components/ArticlesPublications.jsx
+++ app/src/components/ArticlesPublications.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import CoverPrimary from './Shared/CoverPrimary';
 import Footer from './Shared/Footer';
 import Loader from './Shared/Loader';
+import { Link } from 'react-router';
+import Rhombus from './Shared/Rhombus';
+import classnames from 'classnames';
+import AppStyles from '../../styles/application.scss';
+import PubsArticlesLayoutStyles from '../../styles/layout/l-publications-articles.scss';
+import PubArticleStyle from '../../styles/components/c-publication-article.scss';
 
 class ArticlesPublications extends Component {
 
@@ -11,9 +17,31 @@ class ArticlesPublications extends Component {
 
   renderElement(articlePublication) {
     return (
-      <article key={articlePublication.link}>
-        <h4><a href={articlePublication.link} target="_blank">{articlePublication.title}</a></h4>
-        <span>{articlePublication.subtitle}</span>
+      <article
+        className={PubArticleStyle['c-publication-article']}
+        key={articlePublication.link}
+      >
+        <h4 className={PubArticleStyle.title}>
+          <a href={articlePublication.link} target="_blank">
+            {articlePublication.title}
+          </a>
+        </h4>
+        <div className={PubArticleStyle.description}>
+          {articlePublication &&
+            <p>{articlePublication.author}</p>}
+          {articlePublication.date &&
+            <p>{articlePublication.date}</p>}
+        </div>
+        <div className={PubArticleStyle['link-container']}>
+          <Rhombus direction="-right">
+            <Link
+              to={articlePublication.link}
+              target="_blank"
+            >
+              find out more
+            </Link>
+          </Rhombus>
+        </div>
       </article>
     );
   }
@@ -23,26 +51,40 @@ class ArticlesPublications extends Component {
     let pageContent = [];
 
     if (articlesPublications && articlesPublications.articles) {
-      const articles = articlesPublications.articles.map(this.renderElement);
+      const publications = articlesPublications.publications.map(this.renderElement);
 
-      if (articles && articles.length) {
+      if (!!publications && publications.length) {
         pageContent.push(
-          <section key="articles">
-            <h2>Articles</h2>
-            {articles}
+          <section key="publications">
+            <h2
+              className={classnames(AppStyles['section-title'],
+              PubsArticlesLayoutStyles['section-title'])}
+            >
+              Publications
+            </h2>
+            <div className={PubsArticlesLayoutStyles.grid}>
+              {publications}
+            </div>
           </section>
         );
       }
     }
 
     if (articlesPublications && articlesPublications.publications) {
-      const publications = articlesPublications.publications.map(this.renderElement);
+      const articles = articlesPublications.articles.map(this.renderElement);
 
-      if (publications && publications.length) {
+      if (!!articles && articles.length) {
         pageContent.push(
-          <section key="publications">
-            <h2>Publications</h2>
-            {publications}
+          <section key="news">
+            <h2
+              className={classnames(AppStyles['section-title'],
+              PubsArticlesLayoutStyles['section-title'])}
+            >
+              Articles
+            </h2>
+            <div className={PubsArticlesLayoutStyles.grid}>
+              {articles}
+            </div>
           </section>
         );
       }
@@ -58,9 +100,11 @@ class ArticlesPublications extends Component {
         subtitle="lorem ipsum"
         backgroundImageIndex={2}
       />
-      <section>
-        {pageContent}
-      </section>
+      <div className={PubsArticlesLayoutStyles['l-publications-news']}>
+        <div className={AppStyles.wrap}>
+          {pageContent}
+        </div>
+      </div>
       <Footer />
     </div>);
   }
