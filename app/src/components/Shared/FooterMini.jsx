be5b11694b471d418fb4fbc19889cabf54d65a2a
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-08-22 11:26:00 +0200

    add new oceana logo and new links on footer map

--- app/src/components/Shared/FooterMini.jsx
+++ app/src/components/Shared/FooterMini.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styles from '../../../styles/components/shared/c-footer-mini.scss';
-import logooceana from '../../../assets/logos/oceana_logo.png';
+import logooceana from '../../../assets/logos/oceana_logo_white.png';
 import logosky from '../../../assets/logos/skytruth_logo.jpg';
 import logogoogle from '../../../assets/logos/google_logo.png';
 import Footer from './Footer';
@@ -40,7 +40,7 @@ class FooterMini extends Component {
       <div>
         {expandedFooter}
         <footer className={styles['c-footer-mini']}>
-          <div className={styles.partners}>
+          <div className={styles['contain-partners']}>
             <img className={styles.partner} src={logooceana} alt="oceana logo" />
             <img className={styles.partner} src={logosky} alt="skytruth logo" />
             <img className={styles.partner} src={logogoogle} alt="google logo" />
@@ -48,6 +48,12 @@ class FooterMini extends Component {
 
           <ul className={styles.links}>
             <li><a onClick={() => this.toggleFooter()}>{toggleLabel}</a></li>
+            <li><a href="https://carto.com/" target="_blank">CartoDB</a></li>
+            <li><a href="#" target="_blank">Map data ©2016 Google, INEGI</a></li>
+            <li><a href="#" target="_blank">
+              500km <div className={styles['bar-hor']}></div>
+            </a></li>
+            <li><a href="#" target="_blank">Terms of Use</a></li>
           </ul>
         </footer>
       </div>
