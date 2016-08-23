7a41141230d2cef53a68162e3f29eab142d5952b
Tiago Garcia <tiagojsag@gmail.com>
2016-08-23 21:37:42 +0200

    Update map login modal message

--- app/src/components/Map/NoLogin.jsx
+++ app/src/components/Map/NoLogin.jsx
@@ -12,11 +12,8 @@ class NoLogin extends Component {
         </p>
         <h2 className={styles.title}>Login required</h2>
         <p className={styles.content}>
-          You must log in to view the Map. If you are a registered user,&nbsp;
-          <a onClick={this.props.login}>log in here</a>
-        </p>
-        <p className={styles.content}>
-          If you are not a registered user, <Link to="/">sign-up here</Link> to view the Map.
+          To view the Map, you must have an user account. Click&nbsp;
+          <a onClick={this.props.login}>here</a> to login or register.
         </p>
       </div>
     );
