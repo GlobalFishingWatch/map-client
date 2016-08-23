5b2422e99f539a61555c27115977d7c21c521982
Hector Arce de las Heras <Hector.arce@iMac-de-Hector.local>
2016-08-23 18:10:36 +0200

    create styles for tutorial page

--- app/src/components/Tutorials.jsx
+++ app/src/components/Tutorials.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import CoverPrimary from './Shared/CoverPrimary';
 import Footer from './Shared/Footer';
+import baseStyle from '../../styles/_base.scss';
+import tutorialStyle from '../../styles/components/c-tutorial-page.scss';
 
 class Tutorials extends Component {
 
@@ -11,9 +13,21 @@ class Tutorials extends Component {
         subtitle="View our tutorial video to see Global Fishing in action."
         backgroundImageIndex={7}
       />
-      <section>
-        <p>Click on the video below to watch a brief tutorial of how Global Fishing Watch works. </p>
-      </section>
+      <div className={baseStyle.wrap}>
+        <section className={tutorialStyle['c-tutorial-page']}>
+          <p>Click on the video below to watch a brief tutorial of how Global Fishing Watch works. </p>
+          <div className={tutorialStyle['video-container']}>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/sI9OTeTGcrw"
+              frameBorder="0"
+              allowFullScreen
+            >
+            </iframe>
+          </div>
+        </section>
+      </div>
       <Footer />
     </div>);
   }
