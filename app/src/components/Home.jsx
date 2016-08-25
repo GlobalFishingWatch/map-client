3f98b8daa5ab79dd20e54ff3c0e6e19f468dc78e
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2016-08-25 16:41:20 +0200

    moved step list to homepage

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import home from '../../styles/index.scss';
 import Footer from './Shared/Footer';
-import InfoWeb from './Home/InfoWeb';
+import Steps from './Home/Steps';
 import CoverPage from './Home/CoverPage';
 import InfoMap from './Home/InfoMap';
+import HomepageStyles from '../../styles/layout/l-homepage.scss';
 
 class Home extends Component {
   render() {
-    return (<div>
-      <CoverPage />
-      <InfoWeb />
-      <section className={home.infomap}>
-        <InfoMap />
-      </section>
-      <Footer />
-    </div>);
+    return (
+      <div classsName={HomepageStyles['l-homepage']}>
+        <CoverPage />
+        <Steps />
+        <section className={home.infomap}>
+          <InfoMap />
+        </section>
+        <Footer />
+      </div>);
   }
 
 }
