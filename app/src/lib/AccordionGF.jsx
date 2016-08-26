c6841464c510c82c518379d41a05a21059054fb7
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2016-08-26 16:47:12 +0200

    disabled function temporaly

--- app/src/lib/AccordionGF.jsx
+++ app/src/lib/AccordionGF.jsx
@@ -13,14 +13,17 @@ class AccordionGF extends Accordion {
     if(!el.length > 0) return;
     scrollTo(el);
   }
-  componentDidUpdate() {
-    const el = document.getElementsByClassName('react-sanfona-item-expanded');
-    if(!el.length > 0) return;
 
-    // adds timeout to give time to the accordion to close last item-title
-    // and recalculate height properly
-    setTimeout(() => scrollTo(el), 300);
-  }
+  // disabled temporaly
+  // componentDidUpdate() {
+  //   console.log('componentDidUpdate');
+  //   const el = document.getElementsByClassName('react-sanfona-item-expanded');
+  //   if(!el.length > 0) return;
+  //
+  //   // adds timeout to give time to the accordion to close last item-title
+  //   // and recalculate height properly
+  //   setTimeout(() => scrollTo(el), 300);
+  // }
 }
 
 export default AccordionGF;
