f62305ffdba1b51ee40eac2569ee065966dd7b29
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2016-08-26 18:39:14 +0200

    fixed link

--- app/src/components/Shared/ToolTip.jsx
+++ app/src/components/Shared/ToolTip.jsx
@@ -9,7 +9,7 @@ class ToolTip extends Component {
   render() {
     return (
       <Link
-        to="/definitions/fishing"
+        to="/definitions/fishing-effort"
         className={ToolTipStyle['c-tooltip-info']}
       >
         <img src={this.props.iconColor === 'black' ? ImgBlack : ImgWhite} alt="info icon"></img>
