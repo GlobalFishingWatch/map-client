8dc9560ce8e35ef01f5a0a94034a8c29e0c9f09b
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-08-26 16:59:25 +0200

    Forces the app to scroll to top when routing
    The "shouldUpdateScroll" function can be customized to disable the feature. The array "regexes" specifies the routes for which, when the app routes from page A to itself (but eventually with different params), the site isn't scrolled to top.

--- app/src/routes.jsx
+++ app/src/routes.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { useScroll } from 'react-router-scroll';
 import AppContainer from './containers/App';
 import HomeContainer from './components/Home';
 import AuthMapContainer from './containers/AuthMap';
@@ -11,35 +12,113 @@ import TheProject from './components/TheProject';
 import Partners from './components/Partners';
 import TermsOfUse from './components/TermsOfUse';
 import PrivacyPolicy from './components/PrivacyPolicy';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute, applyRouterMiddleware } from 'react-router';
 import ContactUsContainer from './containers/ContactUs';
+
+/**
+ * Return whether the page should be scrolled to top when
+ * routing from one to another
+ *
+ * @param {Object} prevRouterProps
+ * @param {Object} { location }
+ * @returns {Boolean}
+ */
+function shouldUpdateScroll(prevRouterProps, { location }) {
+  /**
+   * Return whether the two pages match the regex and have the same matching
+   * regex parameters
+   * @param  {regex}  regex
+   * @return {Boolean}
+   */
+  function isSamePage(regex) {
+    const pathname = (prevRouterProps && prevRouterProps.location.pathname) || '';
+    const nextPathname = location.pathname;
+
+    /* We first check if the pages are concerned by the regex. If not, the route
+     * isn't matching */
+    const isPathnameConcerned = regex.test(pathname);
+    const isNextPathnameConcerned = regex.test(nextPathname);
+
+    if (!isPathnameConcerned || !isNextPathnameConcerned) {
+      return false;
+    }
+
+    /* We then get the matching regex params and return false if there isn't
+     * any */
+    const routeParams = pathname.match(regex);
+    const nextRouteParams = nextPathname.match(regex);
+
+    if (!routeParams || !nextRouteParams) {
+      return false;
+    }
+
+    /* We remove the first element of the arrays as it is the whole matched
+     * string (i.e. the route) */
+    if (routeParams.length) {
+      routeParams.splice(0, 1);
+    }
+    if (nextRouteParams.length) {
+      nextRouteParams.splice(0, 1);
+    }
+
+    const paramsCount = Math.min(routeParams.length, nextRouteParams.length);
+
+    let doesParamsMatch = true;
+    for (let i = 0, j = paramsCount; i < j; i++) {
+      if (routeParams[i] !== nextRouteParams[i]) {
+        doesParamsMatch = false;
+        break;
+      }
+    }
+
+    return doesParamsMatch;
+  }
+
+  /* Here we define all the routes for which we don't want to scroll to top if
+   * both the old path and the new one match (i.e. if the global regex and the
+   * regex params match the two paths) */
+  const regexes = [];
+
+  for (let i = 0, j = regexes.length; i < j; i++) {
+    if (isSamePage(regexes[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 class Routes extends Component {
   render() {
-    return (<Router history={this.props.history}>
-      <Route path="/" component={AppContainer}>
-        <IndexRoute component={HomeContainer} />
+    return (
+      <Router
+        history={this.props.history}
+        render={applyRouterMiddleware(useScroll(shouldUpdateScroll))}
+      >
+        <Route path="/" component={AppContainer}>
+          <IndexRoute component={HomeContainer} />
 
-        <Route path="map" component={AuthMapContainer} />
+          <Route path="map" component={AuthMapContainer} />
 
-        <Route path="articles-publications" component={ArticlesPublications} />
+          <Route path="articles-publications" component={ArticlesPublications} />
 
-        <Route path="faq" component={FAQContainer} />
-        <Route path="tutorials" component={Tutorials} />
-        <Route path="definitions" component={Definitions}>
-          <Route path=":term" component={Definitions} />
+          <Route path="faq" component={FAQContainer} />
+          <Route path="tutorials" component={Tutorials} />
+          <Route path="definitions" component={Definitions}>
+            <Route path=":term" component={Definitions} />
+          </Route>
+
+          <Route path="the-project" component={TheProject} />
+          <Route path="partners" component={Partners} />
+          <Route path="contact-us" component={ContactUsContainer} />
+
+          <Route path="terms-of-use" component={TermsOfUse} />
+          <Route path="privacy-policy" component={PrivacyPolicy} />
+
+          <Route path="orbcomm" component={Orbcomm} />
         </Route>
-
-        <Route path="the-project" component={TheProject} />
-        <Route path="partners" component={Partners} />
-        <Route path="contact-us" component={ContactUsContainer} />
-
-        <Route path="terms-of-use" component={TermsOfUse} />
-        <Route path="privacy-policy" component={PrivacyPolicy} />
-
-        <Route path="orbcomm" component={Orbcomm} />
-      </Route>
-    </Router>);
+      </Router>
+    );
   }
 }
 
