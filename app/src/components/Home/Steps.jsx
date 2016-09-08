a1553db34279d2f8d3e5ad3ce72320c2b50ae7e8
Tiago Garcia <tiagojsag@gmail.com>
2016-09-08 19:49:10 +0200

    Minor copy changes

--- app/src/components/Home/Steps.jsx
+++ app/src/components/Home/Steps.jsx
@@ -35,8 +35,9 @@ class Steps extends Component {
               </div>
               <div className={StepsStyle.content}>
                 <h3 className={StepsStyle.name}>Step 1: Satellites</h3>
-                <p>Over the course of the year, more than 200,000 different vessels,
-                  including more than 35,000 known or likely commercial fishing vessels,
+                <p>
+                  Over the course of the year, more than 200,000 different vessels,
+                  including more than 35,000 (and counting) known or likely commercial fishing vessels,
                   broadcast their position, course and speed through AIS. Every day, a fleet
                   of satellites records these broadcasts and beams the information down to Earth.
                 </p>
