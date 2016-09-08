bc906e9a706ec8f7ab2f2f793bef7019d42d3669
Hector Arce <hector.arce@vizzuality.com>
2016-09-08 16:28:34 +0200

    update styles for no-login modal on map page

--- app/src/components/Map/NoLogin.jsx
+++ app/src/components/Map/NoLogin.jsx
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from '../../../styles/components/map/c-no-login.scss';
+import ButtonStyle from '../../../styles/components/c-button.scss';
 
 class NoLogin extends Component {
 
   render() {
     return (
       <div className={styles['c-no-login']}>
-        <p className={styles.back}>
-          <Link to="/">￩ Visit Home Page</Link>
-        </p>
         <h2 className={styles.title}>Login required</h2>
         <p className={styles.content}>
-          To view the Map, you must have a user account. Click{' '}
-          <a onClick={this.props.login}>here</a> to login or register.
+          To view the Map, you must have a user account.
         </p>
+        <button
+          type="button"
+          className={ButtonStyle['c-btn-primary']}
+          onClick={this.props.login}
+        >Login</button>
+        <button
+          type="button"
+          className={ButtonStyle['c-btn-primary']}
+          onClick={this.props.login}
+        >Register</button>
+        <p><Link to="/" className={styles.content}>Go home page</Link></p>
       </div>
     );
   }
