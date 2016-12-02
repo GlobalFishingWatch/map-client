610c54e2b633e34c60494f432c2b0c2ee2c3e2b7
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2016-12-02 20:58:45 +0100

    Footer as React Component

--- app/src/components/Shared/Footer.jsx
+++ app/src/components/Shared/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 import classnames from 'classnames';
 
@@ -15,115 +15,129 @@ import FacebookIcon from 'babel!svg-react!assets/icons/facebook.svg?name=Faceboo
 import TwitterIcon from 'babel!svg-react!assets/icons/twitter.svg?name=TwitterIcon';
 import SubscribeIcon from 'babel!svg-react!assets/icons/subscribe.svg?name=SubscribeIcon';
 
-export default function () {
-  return (
-    <footer className={FooterStyles['c-footer']}>
-      <div className={BaseStyles.wrap}>
-        <div className={FooterStyles['logos-footer']}>
-          <div className={FooterStyles['partner-section']}>
-            <span className={FooterStyles['partner-text']}>Founding Partners</span>
-            <ul className={FooterStyles['logo-list']}>
-              <li className={FooterStyles['logo-item']}>
-                <img
-                  className={classnames(FooterStyles.logo, FooterStyles['-oceana'])}
-                  src={OceanaLogo}
-                  alt="Oceana"
-                />
-              </li>
-              <li className={FooterStyles['logo-item']}>
-                <img
-                  className={classnames(FooterStyles.logo, FooterStyles['-skytruth-logo'])}
-                  src={SkytruthLogo}
-                  alt="Skytruth"
-                />
-              </li>
-              <li className={FooterStyles['logo-item']}>
-                <img
-                  className={FooterStyles.logo}
-                  src={GoogleLogo}
-                  alt="Google"
-                />
-              </li>
-            </ul>
-          </div>
-          <div className={FooterStyles['funder-section']}>
-            <ul className={FooterStyles['logo-funder-list']}>
-              <li className={FooterStyles['logo-item']}>
-                <LDFLogo className={classnames(IconStyles.icon, FooterStyles['logo-ldf'])} />
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className={FooterStyles['map-section']}>
-          <ul className={FooterStyles['nav-list']}>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/map">Map</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <a href={BLOG_URL} target="_blank" className={FooterStyles['nav-link']}>Blog</a>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/articles-publications">Articles and Publications</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/faq">FAQ</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/tutorials">Tutorials</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/definitions">Definitions</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/the-project">The project</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/partners">Partners</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/research-program">Research program</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/contact-us">Contact us</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/terms-of-use">Terms of use</Link>
-            </li>
-            <li className={FooterStyles['nav-item']}>
-              <Link className={FooterStyles['nav-link']} to="/privacy-policy">Privacy policy</Link>
-            </li>
-          </ul>
-          <div className={FooterStyles['social-section']}>
-            <a
-              className={FooterStyles['button-subscribe']}
-              href="http://info.globalfishingwatch.org/catch_signup"
-              target="_blank"
-            >
-              sign up for email updates!
-              <SubscribeIcon className={classnames(IconStyles.icon, FooterStyles['icon-subscribe'])} />
-            </a>
-            <div className={FooterStyles['social-links']}>
-              <span className={FooterStyles['social-text']}>Follow us</span>
-              <ul className={FooterStyles['social-list']}>
-                <li className={FooterStyles['social-item']}>
-                  <a href="https://twitter.com/GlobalFishWatch" target="_blank">
-                    <TwitterIcon className={classnames(IconStyles.icon, FooterStyles['icon-twitter'])} />
-                  </a>
+class Footer extends Component {
+
+  render() {
+    return (
+      <footer className={FooterStyles['c-footer']}>
+        <div className={BaseStyles.wrap}>
+          <div className={FooterStyles['logos-footer']}>
+            <div className={FooterStyles['partner-section']}>
+              <span className={FooterStyles['partner-text']}>Founding Partners</span>
+              <ul className={FooterStyles['logo-list']}>
+                <li className={FooterStyles['logo-item']}>
+                  <img
+                    className={classnames(FooterStyles.logo, FooterStyles['-oceana'])}
+                    src={OceanaLogo}
+                    alt="Oceana"
+                  />
                 </li>
-                <li className={FooterStyles['social-item']}>
-                  <a href="https://www.facebook.com/GlobalFishingWatch/" target="_blank">
-                    <FacebookIcon className={classnames(IconStyles.icon, FooterStyles['icon-facebook'])} />
-                  </a>
+                <li className={FooterStyles['logo-item']}>
+                  <img
+                    className={classnames(FooterStyles.logo, FooterStyles['-skytruth-logo'])}
+                    src={SkytruthLogo}
+                    alt="Skytruth"
+                  />
+                </li>
+                <li className={FooterStyles['logo-item']}>
+                  <img
+                    className={FooterStyles.logo}
+                    src={GoogleLogo}
+                    alt="Google"
+                  />
+                </li>
+              </ul>
+            </div>
+            <div className={FooterStyles['funder-section']}>
+              <ul className={FooterStyles['logo-funder-list']}>
+                <li className={FooterStyles['logo-item']}>
+                  <LDFLogo className={classnames(IconStyles.icon, FooterStyles['logo-ldf'])} />
                 </li>
               </ul>
             </div>
           </div>
+          <div className={FooterStyles['map-section']}>
+            <ul className={FooterStyles['nav-list']}>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/map">Map</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <a href={BLOG_URL} target="_blank" className={FooterStyles['nav-link']}>Blog</a>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/articles-publications">Articles and Publications</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/faq">FAQ</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/tutorials">Tutorials</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/definitions">Definitions</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/the-project">The project</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/partners">Partners</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/research-program">Research program</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/contact-us">Contact us</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/terms-of-use">Terms of use</Link>
+              </li>
+              <li className={FooterStyles['nav-item']}>
+                <Link className={FooterStyles['nav-link']} to="/privacy-policy">Privacy policy</Link>
+              </li>
+            </ul>
+            <div className={FooterStyles['social-section']}>
+              <a
+                className={FooterStyles['button-subscribe']}
+                href="http://info.globalfishingwatch.org/catch_signup"
+                target="_blank"
+              >
+                sign up for email updates!
+                <SubscribeIcon className={classnames(IconStyles.icon, FooterStyles['icon-subscribe'])} />
+              </a>
+              <div className={FooterStyles['social-links']}>
+                <span className={FooterStyles['social-text']}>Follow us</span>
+                <ul className={FooterStyles['social-list']}>
+                  <li className={FooterStyles['social-item']}>
+                    <a href="https://twitter.com/GlobalFishWatch" target="_blank">
+                      <TwitterIcon className={classnames(IconStyles.icon, FooterStyles['icon-twitter'])} />
+                    </a>
+                  </li>
+                  <li className={FooterStyles['social-item']}>
+                    <a href="https://www.facebook.com/GlobalFishingWatch/" target="_blank">
+                      <FacebookIcon className={classnames(IconStyles.icon, FooterStyles['icon-facebook'])} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={FooterStyles['sub-footer']}>
-        <img className={FooterStyles['logo-gfw']} src={GFWLogo} alt="Global Fishing Watch" />
-        <span className={FooterStyles['sub-footer-text']}>Global Fishing Watch</span>
-      </div>
-    </footer>
-  );
+        {!this.props.isMap &&
+          <div className={FooterStyles['sub-footer']}>
+            <img className={FooterStyles['logo-gfw']} src={GFWLogo} alt="Global Fishing Watch" />
+            <span className={FooterStyles['sub-footer-text']}>Global Fishing Watch</span>
+          </div>}
+      </footer>
+    );
+  }
 }
+
+Footer.propTypes = {
+  isMap: React.PropTypes.bool
+};
+
+Footer.defaultProps = {
+  isMap: false
+};
+
+export default Footer;
