3df811f44e82736cea7681cdb9713edeea3fdcc1
Andrés González <andres.gonzalez@vizzuality.com>
2016-12-22 10:45:05 +0100

    Fixed no-login modal styles (#424)
    * Fixed no-login modal styles

--- app/src/components/Map/NoLogin.jsx
+++ app/src/components/Map/NoLogin.jsx
@@ -7,14 +7,13 @@ class NoLogin extends Component {
   render() {
     return (
       <div className={styles['c-no-login']}>
-        <p className={styles.back}>
-          <Link to="/">￩ Visit Home Page</Link>
-        </p>
-        <h2 className={styles.title}>Login required</h2>
-        <p className={styles.content}>
-          To view the Map, you must have a user account. Click{' '}
-          <a onClick={this.props.login}>here</a> to login or register.
-        </p>
+        <h2 className={styles.title}>Access needed</h2>
+        <div className={styles.content}>
+          <p>To view the Map, you must have a user account. Click below to access, or visit our{' '}
+            <Link to="/" className={styles.back}>Home Page</Link>
+          </p>
+          <a className={styles['btn-action']} onClick={this.props.login}>log in / register</a>
+        </div>
       </div>
     );
   }
@@ -24,6 +23,5 @@ class NoLogin extends Component {
 NoLogin.propTypes = {
   login: React.PropTypes.func
 };
-
 
 export default NoLogin;
