9da24d2dae384354995d85c285f282555209a658
Andrés González Muñoz <andres.gnlez@gmail.com>
2016-12-22 15:43:20 +0100

    Toggle panel styles and functionality

--- app/src/components/Map/ReportPanel.jsx
+++ app/src/components/Map/ReportPanel.jsx
@@ -1,8 +1,27 @@
+/* eslint-disable max-len  */
+
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
 import ReportPanelStyles from 'styles/components/map/c-report-panel.scss';
 
 class ReportPanel extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible: this.props.polygons.length,
+      expanded: this.props.polygons.length
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      visible: nextProps.polygons.length,
+      expanded: nextProps.polygons.length
+    });
+  }
 
   onRemovePolygon(event) {
     const id = event.currentTarget.getAttribute('id');
@@ -10,13 +29,27 @@ class ReportPanel extends Component {
     this.props.onRemovePolygon(id);
   }
 
+  onTogglePanel() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
+
   render() {
     const polygonItems = [];
+
+    if (!this.state.visible) return null;
+
+    const panelClass = this.state.expanded ?
+      ReportPanelStyles['c-report-panel'] : classnames(ReportPanelStyles['c-report-panel'], ReportPanelStyles['-minimized']);
+
+    const toggleClass = this.state.expanded ?
+      ReportPanelStyles.toggle : classnames(ReportPanelStyles.toggle, ReportPanelStyles['-expanded']);
 
     if (!!this.props.polygons && this.props.polygons.length) {
       this.props.polygons.map(polygon => (
         polygonItems.push((
-          <li className={ReportPanelStyles['polygon-item']}>
+          <li className={ReportPanelStyles['polygon-item']} key={polygon.id}>
             <span className={ReportPanelStyles['polygon-name']}>{polygon.name}</span>
             <span id={polygon.id} className={ReportPanelStyles['polygon-remove']}></span>
           </li>
@@ -25,10 +58,10 @@ class ReportPanel extends Component {
     }
 
     return (
-      <div className={ReportPanelStyles['c-report-panel']}>
+      <div className={panelClass}>
         <div className={ReportPanelStyles.menu}>
           <span className={ReportPanelStyles['report-total']}>{this.props.polygons.length} layers added</span>
-          <span className={ReportPanelStyles.toggle} />
+          <span className={toggleClass} onClick={() => this.onTogglePanel()} />
         </div>
         <div className={ReportPanelStyles.content}>
           {this.props.polygons.length &&
