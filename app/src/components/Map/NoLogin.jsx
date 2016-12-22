761bff1400a1d075da81df2ef7461955fcf14c29
Tiago Garcia <tiagojsag@gmail.com>
2016-12-22 10:44:42 +0100

    Review styles

--- app/src/components/Map/NoLogin.jsx
+++ app/src/components/Map/NoLogin.jsx
@@ -9,9 +9,10 @@ class NoLogin extends Component {
       <div className={styles['c-no-login']}>
         <h2 className={styles.title}>Access needed</h2>
         <div className={styles.content}>
-          <p>To view the Map, you must have a user account.</p>
+          <p>To view the Map, you must have a user account. Click below to access, or visit our{' '}
+            <Link to="/" className={styles.back}>Home Page</Link>
+          </p>
           <a className={styles['btn-action']} onClick={this.props.login}>log in / register</a>
-          <p className={styles['paragraph-back']}>Or visit our <Link to="/" className={styles.back}>Home Page</Link></p>
         </div>
       </div>
     );
@@ -22,6 +23,5 @@ class NoLogin extends Component {
 NoLogin.propTypes = {
   login: React.PropTypes.func
 };
-
 
 export default NoLogin;
