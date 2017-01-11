261d4352e42d6ee080631a5f3850fbc59b23dc33
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-01-11 17:25:05 +0100

    Merges workspace settings layer with library ones

--- app/src/actions/layers.js
+++ app/src/actions/layers.js
@@ -1,18 +1,41 @@
 import _ from 'lodash';
 import { LAYER_TYPES } from 'constants';
-import {
-  SET_LAYERS,
-  TOGGLE_LAYER_VISIBILITY,
-  SET_LAYER_OPACITY,
-  SET_LAYER_HUE
-} from 'actions';
+import { SET_LAYERS, TOGGLE_LAYER_VISIBILITY, SET_LAYER_OPACITY, SET_LAYER_HUE } from 'actions';
 import { updateFlagFilters } from 'actions/filters';
 
-export function initLayers(layers_) {
+export function initLayers(workspaceLayers, libraryLayers) {
   return (dispatch, getState) => {
     const state = getState();
+    const workspaceLayersIds = [];
 
-    let layers = layers_
+    // Get all ids coming from workspace
+    workspaceLayers.forEach((l) => {
+      if (l.id === undefined) {
+        return;
+      }
+      workspaceLayersIds.push(l.id);
+    });
+
+    // formats layer object to keep a consistent format around the app
+    libraryLayers.forEach((l) => {
+      // moves "args" content to the root of the object
+      Object.assign(l, l.args);
+      // removes "args" property from the object
+      /* eslint no-param-reassign: 0 */
+      delete l.args;
+    });
+
+    // Match workspace ids with library ones
+    const matchedLayers = _.filter(libraryLayers, (l) => workspaceLayersIds.indexOf(l.id) !== -1);
+
+    matchedLayers.forEach((l) => {
+      const localLayer = _.find(workspaceLayers, (wl) => wl.id === l.id);
+
+      // overwrites API values with workspace ones
+      Object.assign(l, localLayer);
+    });
+
+    let layers = matchedLayers
       .filter(l => _.values(LAYER_TYPES).indexOf(l.type) !== -1);
 
     if (state.user.userPermissions.indexOf('seeVesselsLayers') === -1) {
@@ -29,38 +52,24 @@ export function initLayers(layers_) {
       }
     });
 
-    // add an id to each layer
-    let id = 0;
-    layers.forEach(layer => {
-      /* eslint no-param-reassign: 0 */
-      layer.id = id;
-      id++;
-    });
-
     dispatch({
-      type: SET_LAYERS,
-      payload: layers
+      type: SET_LAYERS, payload: layers
     });
   };
 }
 
-
 export function toggleLayerVisibility(layerId, forceShow = false) {
   return {
-    type: TOGGLE_LAYER_VISIBILITY,
-    payload: {
-      layerId,
-      forceShow
+    type: TOGGLE_LAYER_VISIBILITY, payload: {
+      layerId, forceShow
     }
   };
 }
 
 export function setLayerOpacity(opacity, layerId) {
   return {
-    type: SET_LAYER_OPACITY,
-    payload: {
-      layerId,
-      opacity
+    type: SET_LAYER_OPACITY, payload: {
+      layerId, opacity
     }
   };
 }
@@ -68,10 +77,8 @@ export function setLayerOpacity(opacity, layerId) {
 export function setLayerHue(hue, layerId) {
   return (dispatch) => {
     dispatch({
-      type: SET_LAYER_HUE,
-      payload: {
-        layerId,
-        hue
+      type: SET_LAYER_HUE, payload: {
+        layerId, hue
       }
     });
     // TODO we might want to override all filters hue settings here (see with Dani)
