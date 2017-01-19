1009d1ed6c45aa3693ed593ef3ec13c06f9a8fff
Tiago Garcia <tiagojsag@gmail.com>
2017-01-19 19:39:53 +0100

    Fix issue on updating vessel info details

--- app/src/containers/Map/SearchResult.js
+++ app/src/containers/Map/SearchResult.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
 import SearchResult from 'components/Map/SearchResult';
-import { addVessel } from 'actions/vesselInfo';
+import { addVessel, clearVesselInfo } from 'actions/vesselInfo';
 
 const mapDispatchToProps = (dispatch) => ({
   drawVessel: (vesselDetails) => {
+    dispatch(clearVesselInfo());
     dispatch(addVessel(vesselDetails.seriesgroup, null, true));
   }
 });
