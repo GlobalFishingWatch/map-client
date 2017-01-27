a6c092c2d1135f65aec69bf6eb055b433c7b5da3
Tiago Garcia <tiagojsag@gmail.com>
2017-01-27 16:14:37 +0100

    Improve layer delete modal styling

--- app/src/components/Map/PromptLayerRemoval.jsx
+++ app/src/components/Map/PromptLayerRemoval.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
-import recentVesselStyles from 'styles/components/map/c-recent-vessels.scss';
 import ModalStyles from 'styles/components/shared/c-modal.scss';
 import PromptLayerStyles from 'styles/components/map/c-prompt-layer.scss';
 import MapButtonStyles from 'styles/components/map/c-button.scss';
 
-
 class PromptLayerRemoval extends Component {
   render() {
     return (
-      <div className={recentVesselStyles['c-recent-vessels']} >
-        <h3 className={ModalStyles['modal-title']} >Delete confirmation</h3>
+      <div >
+        <h3 className={ModalStyles['modal-title']} >Delete layer</h3>
         <div className={PromptLayerStyles['c-prompt-layer']}>
-          <p>
-            You are about to remove a custom layer, which is part of your workspace and cannot be restored once deleted.
+          <p
+            className={PromptLayerStyles['layer-description']}
+          >
+            You are about to remove a custom layer, which is part of your workspace and cannot be restored from
+            the layer library once deleted.
+          </p>
+          <p
+            className={PromptLayerStyles['layer-description']}
+          >
             Are you sure you want to proceed?
           </p>
           <div className={classnames(ModalStyles.footer, PromptLayerStyles.footer)} >
@@ -22,14 +27,13 @@ class PromptLayerRemoval extends Component {
                 PromptLayerStyles['modal-btn'])}
               onClick={() => this.props.removeLayer(this.props.layerIdPromptedForRemoval)}
             >
-              Yes
+              Yes, delete
             </button>
             <button
-              className={classnames(MapButtonStyles['c-button'], MapButtonStyles['-filled'],
-                PromptLayerStyles['modal-btn'])}
+              className={classnames(MapButtonStyles['c-button'], PromptLayerStyles['modal-btn'])}
               onClick={() => this.props.keepLayer()}
             >
-              No
+              Cancel
             </button>
           </div>
         </div>
