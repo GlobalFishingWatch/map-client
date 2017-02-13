93649719b2e8195c1bb121d4fbcdecceba12fdff
erik <erik.escoffier@gmail.com>
2017-02-13 11:04:51 +0100

    temp fix for failing info endpoint

--- app/src/constants.js
+++ app/src/constants.js
@@ -37,8 +37,7 @@ export const VESSELS_ENDPOINT_KEYS = [
 export const GUEST_PERMISSION_SET = [
   'seeVesselsLayers',
   'selectVessel',
-  'shareWorkspace',
-  'seeVesselBasicInfo'
+  'shareWorkspace'
 ];
 
 // for now, auth users get no special permissions, everything comes from the API
