de81a5aa40b07fd58a75f9a3c5299d494b4e4961
Rodrigo Solís <rodrigo.solis@vizzuality.com>
2017-02-16 13:31:13 +0100

    prevents zoom on large spanned custom layers

--- app/src/components/Layers/CustomLayerWrapper.js
+++ app/src/components/Layers/CustomLayerWrapper.js
@@ -1,10 +1,15 @@
+import { MAX_AUTO_ZOOM_LONGITUDE_SPAN } from 'constants';
+
 export default class CustomLayerWrapper {
   constructor(map, url) {
     this.map = map;
     this.kmlLayer = new google.maps.KmlLayer({
       url,
-      map
+      map,
+      preserveViewport: true
     });
+    this.handleZoomLevel = this.handleZoomLevel.bind(this);
+    google.maps.event.addListener(this.kmlLayer, 'defaultviewport_changed', this.handleZoomLevel);
   }
 
   show() {
@@ -13,5 +18,13 @@ export default class CustomLayerWrapper {
 
   hide() {
     this.kmlLayer.setMap(null);
+  }
+
+  handleZoomLevel() {
+    const bounds = this.kmlLayer.getDefaultViewport();
+    const span = bounds.toSpan().toJSON();
+
+    if (span.lng >= MAX_AUTO_ZOOM_LONGITUDE_SPAN) this.map.setCenter(bounds.getCenter());
+    else this.map.fitBounds(bounds.toJSON());
   }
 }
