10886dc04b7fb757c70519c91b09c894fca37645
Tiago Garcia <tiagojsag@gmail.com>
2017-03-02 16:17:25 +0100

    Change return to start behavior on playback

--- app/src/components/Map/Timebar.jsx
+++ app/src/components/Map/Timebar.jsx
@@ -461,16 +461,10 @@ class Timebar extends Component {
     const endOfTime = this.props.timelineOuterExtent[1];
     const isAtEndOfTime = x(offsetInnerExtent[1]) >= x(endOfTime);
 
-
     // if we're at the end of time, just stop playing
     if (isAtEndOfTime) {
       const innerExtentDelta = offsetInnerExtent[1].getTime() - offsetInnerExtent[0].getTime();
       offsetInnerExtent = [new Date(endOfTime.getTime() - innerExtentDelta), endOfTime];
-      if (this.props.timelinePaused === false) {
-        // we're at the end and still playing: rewind, and bail to override inner dates update
-        this.props.rewind();
-        return;
-      }
       this.props.updatePlayingStatus(true);
     }
 
@@ -486,6 +480,16 @@ class Timebar extends Component {
   }
 
   onPauseToggle() {
+    const playStep = this.getPlayStep(this.props.timelineOuterExtent);
+    const realTimePlayStep = Math.max(MIN_FRAME_LENGTH_MS, playStep);
+    const offsetInnerExtent = this.props.timelineInnerExtent.map(d => new Date(d.getTime() + realTimePlayStep));
+    const endOfTime = this.props.timelineOuterExtent[1];
+    const isAtEndOfTime = x(offsetInnerExtent[1]) >= x(endOfTime);
+
+    if (isAtEndOfTime) {
+      this.props.rewind();
+    }
+
     lastTimestamp = null;
     const paused = !this.props.timelinePaused;
     this.props.updatePlayingStatus(paused);
