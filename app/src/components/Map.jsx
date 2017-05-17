5534fbbdcbf01dbc71c73858e236c9b303554c67
erik <erik.escoffier@gmail.com>
2017-05-17 10:06:20 +0200

    switched to Google hybrid style

--- app/src/components/Map.jsx
+++ app/src/components/Map.jsx
@@ -365,7 +365,7 @@ class Map extends Component {
                 maxZoom: this.props.maxZoom,
                 minZoom: MIN_ZOOM_LEVEL
               }}
-              defaultMapTypeId={google.maps.MapTypeId.SATELLITE}
+              defaultMapTypeId={google.maps.MapTypeId.HYBRID}
               onMousemove={this.onMouseMove}
               onZoomChanged={this.onZoomChanged}
               onDragend={this.onDragEnd}
