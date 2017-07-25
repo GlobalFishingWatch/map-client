87cfe0a16ca55c77d543144492d4895f0c315b2b
erik <erik.escoffier@gmail.com>
2017-07-25 12:54:22 +0200

    added North Star basemap

--- app/src/basemap/basemapReducer.js
+++ app/src/basemap/basemapReducer.js
@@ -20,6 +20,13 @@ const initialState = {
       url: 'https://api.mapbox.com/styles/v1/enriquetuya/cj3vr6qy802b72so7jvennfkg/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZW5yaXF1ZXR1eWEiLCJhIjoiY2loNmFwYjJuMDlzZnR4bHh3NnRyNmQxcCJ9.vf_v5i6RWNz5Q7rglf35pQ'
     },
     {
+      title: 'North Star',
+      label: 'North Star',
+      description: 'Custom basemap showing bathymetry, parallels and ports',
+      type: 'Basemap',
+      url: 'https://api.mapbox.com/styles/v1/danielcaso/cj49xom6535sn2spado3w5to6/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGFuaWVsY2FzbyIsImEiOiJjaXVvYXB6M2gwMDF4MnRtd2c1b3lqdmh4In0.-qcuYFkQVE6U-TyfFswH9g'
+    },
+    {
       title: 'High Contrast',
       label: 'High Contrast',
       description: 'High contrast basemap, that highlights borders and shore. Ideal for usage with projectors',
