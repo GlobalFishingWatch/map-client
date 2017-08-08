d25a10dcb783feb59665b8b360cb7c5dffd836ca
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-08-08 18:40:51 +0200

    Terrible typo

--- app/src/reducers/areas.js
+++ app/src/reducers/areas.js
@@ -12,7 +12,7 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case SAVE_AREA:
       if (action.payload.area) {
-        const areas = state.data.lenght > 0 ? state.data : [];
+        const areas = state.data.length > 0 ? state.data : [];
         return Object.assign({}, state, { data: areas.concat([action.payload.area]) });
       }
       return state;
