4d1fab6171382f904bd90e45bb2bf0ea4570780e
Alvaro Leal <aleal@icometrix.com>
2017-08-11 13:40:45 +0200

    Delete area button and functionality

--- app/src/components/Map/AreasPanel.jsx
+++ app/src/components/Map/AreasPanel.jsx
@@ -5,6 +5,7 @@ import AreasForm from 'containers/Map/AreasForm';
 import { COLORS } from 'constants';
 import areasPanelStyles from 'styles/components/map/areas-panel.scss';
 import controlPanelStyles from 'styles/components/control_panel.scss';
+import buttonStyles from 'styles/components/map/button.scss';
 import Toggle from 'components/Shared/Toggle';
 
 class AreasPanel extends Component {
@@ -24,14 +25,20 @@ class AreasPanel extends Component {
               <div key={i} className={classnames(itemClassNames)}>
                 {recentLastArea && <div className={classnames([areasPanelStyles.recentLabel])}>Area added recently</div>}
                 <div className={classnames([areasPanelStyles.areaItem])}>
-                  <Toggle
-                    on={area.visible}
-                    color={COLORS[area.color]}
-                    onToggled={() => toggleAreaVisibility(i)}
-                  />
-                  <div className={classnames([areasPanelStyles.name])}>
-                    { area.name }
+                  <div className={classnames([areasPanelStyles.nameContainer])}>
+                    <Toggle
+                      on={area.visible}
+                      color={COLORS[area.color]}
+                      onToggled={() => toggleAreaVisibility(i)}
+                    />
+                    <div className={classnames([areasPanelStyles.name])}>
+                      { area.name }
+                    </div>
                   </div>
+                  <button
+                    className={classnames([buttonStyles.button, buttonStyles._primary])}
+                    onClick={() => this.props.deleteArea(i)}
+                  >Delete</button>
                 </div>
               </div>
             );
@@ -47,7 +54,8 @@ class AreasPanel extends Component {
 AreasPanel.propTypes = {
   areas: PropTypes.array.isRequired,
   recentlyCreated: PropTypes.bool.isRequired,
-  toggleAreaVisibility: PropTypes.func.isRequired
+  toggleAreaVisibility: PropTypes.func.isRequired,
+  deleteArea: PropTypes.func.isRequired
 };
 
 export default AreasPanel;
