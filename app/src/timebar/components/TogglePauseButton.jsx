cc7824ec4a21a5fd4ecc13c8207e3bcf655e866b
Tiago Garcia <tiagojsag@gmail.com>
2017-08-18 22:55:16 +0200

    Remove unused code on component

--- app/src/timebar/components/TogglePauseButton.jsx
+++ app/src/timebar/components/TogglePauseButton.jsx
@@ -5,25 +5,16 @@ import iconStyles from 'styles/icons.scss';
 import PlayIcon from '-!babel-loader!svg-react-loader!assets/icons/play.svg?name=FacebookIcon';
 
 class TogglePauseButton extends Component {
-  constructor(props) {
-    super(props);
-    this.onToggle = this.onToggle.bind(this);
-  }
-
-  onToggle() {
-    this.props.onToggle();
-  }
-
   render() {
     return (
-      <button onClick={this.props.onToggle}>
+      <button onClick={this.props.onToggle} >
         {this.props.paused &&
         <PlayIcon className={classnames(iconStyles.icon, iconStyles.playIcon)} />
         }
         {!this.props.paused &&
         '❚❚'
         }
-      </button>
+      </button >
     );
   }
 }
