17a3a414ac0a1daff366c9fe37a8817bf5003d6c
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-08-31 16:21:38 +0200

    Add animation to accordion

--- app/src/components/Shared/ExpandItem.jsx
+++ app/src/components/Shared/ExpandItem.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import ExpandItemStyles from 'styles/components/shared/expand-item.scss';
+import { VelocityTransitionGroup } from 'velocity-react';
 
 function ExpandItem({ active, children, arrowPosition, carousel }) {
   return (
     <div
       className={classnames(ExpandItemStyles.expandItem)}
     >
-      <div
-        className={classnames({
-          [ExpandItemStyles.opened]: active === true,
-          [ExpandItemStyles.closed]: active === false,
-          [ExpandItemStyles.notCarousel]: !carousel,
-          [ExpandItemStyles.firstIcon]: arrowPosition === 0,
-          [ExpandItemStyles.secondIcon]: arrowPosition === 1
-        })}
+      <VelocityTransitionGroup
+        enter={{ animation: 'slideDown', duration: 200, easing: 'easeOutCubic' }}
+        leave={{ animation: 'slideUp', duration: 200, easing: 'easeOutCubic' }}
       >
-        {children}
-      </div>
+        {active && <div
+          className={classnames({
+            [ExpandItemStyles.notCarousel]: !carousel,
+            [ExpandItemStyles.firstIcon]: arrowPosition === 0,
+            [ExpandItemStyles.secondIcon]: arrowPosition === 1
+          })}
+        >
+          {children}
+        </div>
+        }
+      </VelocityTransitionGroup>
     </div>
   );
 }
