bdb3ce05f8a39b68605244186104cafd2a01990f
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-09-18 18:31:11 +0200

    Adjust badge for mobile

--- app/src/mapPanels/rightControlPanel/components/MenuLink.jsx
+++ app/src/mapPanels/rightControlPanel/components/MenuLink.jsx
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import MenuLinkStyles from 'styles/components/menu-link.scss';
 import ControlPanelStyles from 'styles/components/control_panel.scss';
+import classnames from 'classnames';
 
 function MenuLink({ title, icon, badge, onClick }) {
   return (
     <div className={MenuLinkStyles.menuLink} onClick={onClick}>
       <div className={MenuLinkStyles.header} >
         <h2 className={MenuLinkStyles.title} >{title}</h2>
-        <div className={MenuLinkStyles.rightIcons} >
+        <div className={classnames(MenuLinkStyles.rightIcons, { [MenuLinkStyles.hasBadge]: badge > 0 })} >
           {(badge > 0) && <div className={MenuLinkStyles.badge} >{badge}</div>}
           <div className={ControlPanelStyles.icon} >{icon}</div>
         </div>
