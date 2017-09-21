9de66a0fa200b5c9aec74dfa8069a683428c6dce
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-09-21 15:22:05 +0200

    Add events for recent vessel added, pinned vessel hue change and recent vessels modal open

--- app/src/recentVessels/containers/RecentVesselItem.js
+++ app/src/recentVessels/containers/RecentVesselItem.js
@@ -3,9 +3,11 @@ import RecentVesselItem from 'recentVessels/components/RecentVesselItem';
 import { addVessel, clearVesselInfo } from 'actions/vesselInfo';
 import { toggleLayerVisibility } from 'layers/layersActions';
 import { setRecentVesselsModalVisibility } from 'recentVessels/recentVesselsActions';
+import { trackRecentVesselAdded } from 'analytics/analyticsActions';
 
 const mapDispatchToProps = dispatch => ({
   drawVessel: (vesselDetails) => {
+    dispatch(trackRecentVesselAdded());
     dispatch(toggleLayerVisibility(vesselDetails.tilesetId, true));
     dispatch(clearVesselInfo());
     dispatch(addVessel(vesselDetails.tilesetId, vesselDetails.seriesgroup, null, true, true));
