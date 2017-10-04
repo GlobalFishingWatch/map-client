3c69eee9b81e2dc947b4d8dea9b4d01661aa9c1b
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-10-04 14:03:00 +0200

    Check for request status instead of response text when vessel not found

--- app/src/actions/vesselInfo.js
+++ app/src/actions/vesselInfo.js
@@ -64,7 +64,7 @@ function setCurrentVessel(tilesetId, seriesgroup, fromSearch) {
       if (request.readyState !== 4) {
         return;
       }
-      if (request.status >= 500 || request.responseText === 'Not Found') {
+      if (request.status >= 500 || request.status === 404) {
         console.error('Error loading vessel info:', request.responseText);
         return;
       }
@@ -219,8 +219,8 @@ export function setPinnedVessels(pinnedVessels) {
         if (request.readyState !== 4) {
           return;
         }
-        if (request.responseText === 'Not Found') {
-          console.warn('vessel info not found');
+        if (request.status === 404) {
+          console.warn('Error:', request.responseText);
           return;
         }
         const data = JSON.parse(request.responseText);
