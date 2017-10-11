3a5c64bb090fed99087a79b9060bb7398b5dbd7e
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-10-11 13:29:53 +0200

    Implement fake backend for encounters - WIP

--- app/src/mapPanels/rightControlPanel/actions/encounters.js
+++ app/src/mapPanels/rightControlPanel/actions/encounters.js
@@ -14,14 +14,38 @@ export function clearEncountersInfo() {
   };
 }
 
-export function setEncountersInfo(data) {
-  return (dispatch) => {
-    // /info is called on the encounters tileset, which results in the following response structure:
-    const fakeEncounter = {
-      duration: data.duration || '10h', // this should be a date object
+
+function get(url, token) {
+  if (typeof XMLHttpRequest === 'undefined') {
+    throw new Error('XMLHttpRequest is disabled');
+  }
+  const request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  if (token) { request.setRequestHeader('Authorization', `Bearer ${token}`); }
+  request.setRequestHeader('Accept', 'application/json');
+  request.onreadystatechange = () => {
+    if (request.readyState !== 4) return;
+    if (request.status >= 500 || request.status === 404) {
+      console.error('Error loading info:', request.responseText);
+    }
+  };
+
+  if (request.responseText === '') {
+    console.error('Error: Empty Response', url);
+    return undefined;
+  }
+
+  return JSON.parse(request.responseText);
+}
+
+function getEncountersInfo(encounterId, token) {
+  // /info is called on the encounters tileset, which results in the following response structure:
+  if (encounterId === 'fakeEncounter') {
+    return {
+      duration: '10h', // same duration format as vessels datetimes
       vessels: [
         {
-          tilesetId: '12222',
+          tilesetId: '429-resample-v1-tms',
           vesselTypeName: 'ENCOUNTER_BOAT',
           seriesgroups: [111222, 222333]
         },
@@ -30,20 +54,59 @@ export function setEncountersInfo(data) {
         }
       ]
     };
+  }
 
-    // For each vessel object in the vessels array:
-    // - if tilesetId is “SELF”:
-    //   - /info is called on the encounters tileset, retrieving data about the vessel
-    //   Query parameters: encounterId (from encounter point)
-    //   - tracks endpoint is called for all available years, on the encounters tileset.
-    //       Query parameters: encounterId(from encounter point)
+  const url = `???/sub/encounterId=${encounterId}/info`;
+  return get(url, token);
+}
 
-    // - else
-    // - /info is called on the tileset with tilesetId, retrieving data about the vessel.
-    //     Query parameters: seriesgroup (from seriesgroup array)
-    // - tracks endpoint is called for all available years, on the tileset with tilesetId.
-    //     Query parameters: seriesgroup (from seriesgroup array)
-      // fake data
+function getVesselInfo(encounterPoint, vessel, token, workspaceLayers) {
+  const tilesetId = vessel ? vessel.tilesetId : 'reefersTilesetId';
+  const layer = workspaceLayers.find(l => l.tilesetId === tilesetId);
+  if (layer === undefined) {
+    console.error(`Error loading vessel info: layer ${vessel.tilesetId} is not found`);
+    return undefined;
+  }
+  const encounterId = vessel.encounterId;
+
+  const url = encounterId !== undefined ?
+    `${layer.url}/sub/seriesgroup=${encounterPoint.encounterId}/info` :
+    `${layer.url}/sub/seriesgroup=${vessel.seriesgroup}/info`;
+  return get(url, token);
+}
+
+
+export function setEncountersInfo(data) {
+  return (dispatch, getState) => {
+    const state = getState();
+    const token = state.user.token;
+
+    const fakeEncounterPointInfo = {
+      encounterId: 'fakeEncounter',
+      datetime: '2018-01-01T00:00:00.000Z',
+      worldY: '-35.4607',
+      worldX: '-36.4603'
+    };
+
+    let encounterPointInfo = data;
+    encounterPointInfo = fakeEncounterPointInfo;
+
+    const encounterId = encounterPointInfo.encounterId;
+    const fakeEncounter = getEncountersInfo(encounterId, token);
+
+    fakeEncounter.vessels.forEach((vessel) => {
+      if (vessel.tilesetId === 'SELF') {
+        getVesselInfo(encounterPointInfo, null, token, state.layers.workspaceLayers);
+        //   - tracks endpoint is called for all available years, on the encounters tileset.
+        //   ${tilesetUrl}/v2/tilesets/${tilesetId}/sub/seriesgroup=${seriesGroup}/${startDate},${endDate};0,0,0
+        //       Query parameters: encounterId(from encounter point)
+      } else {
+        getVesselInfo(encounterPointInfo, vessel, token, state.layers.workspaceLayers);
+        // - tracks endpoint is called for all available years, on the tileset with tilesetId.
+        //   ${tilesetUrl}/v2/tilesets/${tilesetId}/sub/seriesgroup=${seriesGroup}/${startDate},${endDate};0,0,0
+        //     Query parameters: seriesgroup (from seriesgroup array)
+      }
+    });
 
     dispatch({
       type: SET_ENCOUNTERS_INFO,
