8101da94368e9b8bf9f93c143aad0bd1b7a41bd3
Erik Escoffier <erik.escoffier@gmail.com>
2018-02-05 15:11:05 +0100

    display warning if encounters header is incorrect

--- app/src/actions/heatmap.js
+++ app/src/actions/heatmap.js
@@ -486,7 +486,11 @@ export function getVesselFromHeatmap(tileQuery, latLng) {
       const selectedSeriesgroup = foundVessels[0].seriesgroup;
 
       if (layer.subtype === LAYER_TYPES.Encounters) {
-        dispatch(setEncountersInfo(selectedSeries, layer.tilesetId, layer.header.urls.info[0][0]));
+        if (layer.header.urls.info === undefined) {
+          console.warn('Info field is missing on header\'s urls, can\'t display encounters details');
+        } else {
+          dispatch(setEncountersInfo(selectedSeries, layer.tilesetId, layer.header.urls.info[0][0]));
+        }
       } else {
         dispatch(addVessel(layer.tilesetId, selectedSeriesgroup, selectedSeries));
       }
