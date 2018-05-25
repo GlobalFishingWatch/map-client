7da88be816b62790dd4d21a3f9221cde51787ee3
nerik <erik.escoffier@gmail.com>
2018-05-25 10:43:45 +0200

    display humanized datetime in vesselinfo

--- app/src/vesselInfo/components/VesselInfoDetails.jsx
+++ app/src/vesselInfo/components/VesselInfoDetails.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { getCountry } from 'iso-3166-1-alpha-2';
+import moment from 'moment';
+import { FORMAT_DATE, FORMAT_TIME } from 'config';
 
 import infoPanelStyles from 'styles/components/info-panel.scss';
 
@@ -27,7 +29,10 @@ class VesselInfoDetails extends Component {
       if (vesselInfo[field.id] === undefined) {
         return;
       }
-      switch (field.kind) {
+
+      const kind = field.kind || field.id;
+
+      switch (kind) {
         case 'prefixedCSVMultiLink':
           if (!vesselInfo[field.id]) {
             break;
@@ -75,6 +80,16 @@ class VesselInfoDetails extends Component {
             <span className={infoPanelStyles.value} >{getCountry(vesselInfo[field.id]) || '---'}</span>
           </div>);
           break;
+        case 'datetime': {
+          const humanizedDate = vesselInfo[field.id] ?
+            moment(vesselInfo[field.id]).format(`${FORMAT_DATE} ${FORMAT_TIME}`) :
+            null;
+          renderedFieldList.push(<div key={field.id} className={infoPanelStyles.rowInfo}>
+            <span className={infoPanelStyles.key}>{field.display}</span>
+            <span className={infoPanelStyles.value}>{humanizedDate || '---'}</span>
+          </div>);
+          break;
+        }
         default:
           renderedFieldList.push(<div key={field.id} className={infoPanelStyles.rowInfo} >
             <span className={infoPanelStyles.key} >{field.display}</span>
