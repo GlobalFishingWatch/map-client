4c750a1b50b44c02c2b167a565f22b9b6d6d9421
j8seangel <jseangel@gmail.com>
2019-01-31 17:14:16 +0100

    Make redux dev tools usable removing huge reducers

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -87,6 +87,7 @@ const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      stateSanitizer: state => ({ ...state, tracks: 'NOT_SERIALIZED', heatmap: 'NOT_SERIALIZED', heatmapTiles: 'NOT_SERIALIZED' })
     }) : compose;
 
 const enhancer = composeEnhancers(
