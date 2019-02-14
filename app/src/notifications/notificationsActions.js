519213ab83150e1afe3fbcb7bac3ef00f8dc45d1
j8seangel <jseangel@gmail.com>
2019-02-14 17:01:39 +0100

    Update actions to follow arrow functions style

--- app/src/notifications/notificationsActions.js
+++ app/src/notifications/notificationsActions.js
@@ -1,19 +1,15 @@
 export const SET_NOTIFICATION = 'SET_NOTIFICATION';
 
-export function resetNotification() {
-  return {
-    type: SET_NOTIFICATION,
-    payload: {
-      visible: false,
-      content: '',
-      type: 'notification'
-    }
-  };
-}
+export const resetNotification = () => ({
+  type: SET_NOTIFICATION,
+  payload: {
+    visible: false,
+    content: '',
+    type: 'notification'
+  }
+});
 
-export function setNotification(payload) {
-  return {
-    type: SET_NOTIFICATION,
-    payload
-  };
-}
+export const setNotification = payload => ({
+  type: SET_NOTIFICATION,
+  payload
+});
