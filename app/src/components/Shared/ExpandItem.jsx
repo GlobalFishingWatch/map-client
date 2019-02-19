393be5860d7cfe445476d69ca65723f5404baee1
nerik <erik.escoffier@gmail.com>
2019-02-19 12:21:31 +0100

    removed velocity-react

--- app/src/components/Shared/ExpandItem.jsx
+++ app/src/components/Shared/ExpandItem.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import ExpandItemStyles from 'styles/components/shared/expand-item.scss';
-import { VelocityTransitionGroup } from 'velocity-react';
 
 function ExpandItem({ active, children, accordion, isVesselInfoPanelOpen }) {
   return (
@@ -13,19 +12,15 @@ function ExpandItem({ active, children, accordion, isVesselInfoPanelOpen }) {
           { [ExpandItemStyles.isVesselInfoPanelOpen]: isVesselInfoPanelOpen }
         )}
       >
-        <VelocityTransitionGroup
-          enter={{ animation: 'slideDown', duration: 200, easing: 'easeOutCubic' }}
-          leave={{ animation: 'slideUp', duration: 200, easing: 'easeOutCubic' }}
+        {active && <div
+          className={classnames({
+            [ExpandItemStyles.notAccordion]: !accordion
+          })}
         >
-          {active && <div
-            className={classnames({
-              [ExpandItemStyles.notAccordion]: !accordion
-            })}
-          >
-            {children}
-          </div>
-          }
-        </VelocityTransitionGroup>
+          {children}
+        </div>
+        }
+
       </div>
     </div>
   );
