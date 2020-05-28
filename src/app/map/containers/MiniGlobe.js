311b51cf7f937d64908a89e9a9aa98b592f41f37
j8seangel <jseangel@gmail.com>
2020-05-28 18:08:43 +0200

    use new components

--- src/app/map/containers/MiniGlobe.js
+++ src/app/map/containers/MiniGlobe.js
@@ -1,11 +1,16 @@
 import { connect } from 'react-redux'
-import MiniGlobe from '@globalfishingwatch/map-components/components/miniglobe'
+import MiniGlobe from '@globalfishingwatch/ui-components/dist/miniglobe'
 
-const mapStateToProps = (state) => ({
-  center: state.workspace.viewport.center,
-  zoom: state.workspace.viewport.zoom,
-  bounds: state.workspace.viewport.bounds,
-  viewportThickness: 2,
-})
+const mapStateToProps = (state) => {
+  const { viewport } = state.workspace
+  return {
+    center: {
+      latitude: viewport.center && viewport.center[0],
+      longitude: viewport.center && viewport.center[1],
+    },
+    bounds: viewport.bounds,
+    viewportThickness: 2,
+  }
+}
 
 export default connect(mapStateToProps)(MiniGlobe)
